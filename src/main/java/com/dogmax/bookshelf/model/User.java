ae04276911dcb6e18675a7b56268312d403e77b8
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 14:01:38 +0300

    Add User Model

---
+++ src/main/java/com/dogmax/bookshelf/model/User.java
@@ -0,0 +1,18 @@
+package com.dogmax.bookshelf.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String login;
+    private String password;
+    private List<String> roles;
+}
