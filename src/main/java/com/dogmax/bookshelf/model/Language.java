22c1020cbd0f6566fab351c55f6f0ce84f501111
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 15:24:07 +0300

    Change Language model

--- src/main/java/com/dogmax/bookshelf/model/Language.java
+++ src/main/java/com/dogmax/bookshelf/model/Language.java
@@ -1,7 +1,17 @@
 package com.dogmax.bookshelf.model;
 
-public enum Language {
-    EN,
-    US,
-    UA
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String language;
+
 }
