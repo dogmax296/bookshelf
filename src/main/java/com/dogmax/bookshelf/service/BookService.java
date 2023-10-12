766c5cb37b383b4c4d497c4037536977ba626af3
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 15:55:13 +0300

    Add BookService

---
+++ src/main/java/com/dogmax/bookshelf/service/BookService.java
@@ -0,0 +1,18 @@
+package com.dogmax.bookshelf.service;
+
+import com.dogmax.bookshelf.model.Book;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BookService {
+    Book create(Book book);
+
+    List<Book> findAll(Map<String, String> params);
+
+    Book getById(Long id);
+
+    void delete(Long id);
+
+    Book update(Book book);
+}
