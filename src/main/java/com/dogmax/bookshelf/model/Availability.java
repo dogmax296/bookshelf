94b95083ca3a7fbfa3252d061bbb3021cf80de7d
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 14:45:31 +0300

    Change Availability Model

--- src/main/java/com/dogmax/bookshelf/model/Availability.java
+++ src/main/java/com/dogmax/bookshelf/model/Availability.java
@@ -1,7 +1,25 @@
 package com.dogmax.bookshelf.model;
 
 
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public enum Availability {
-    IN_STOCK,
-    OUT_OF_STOCK
+    IN_STOCK(0),
+    OUT_OF_STOCK(1);
+    private final Integer available;
+
+    Availability(Integer available) {
+        this.available = available;
+    }
+
+    public Integer getAvailable() {
+        return available;
+    }
+    public static Availability of(Integer available){
+        return Stream.of(Availability.values())
+                .filter(a -> Objects.equals(a.getAvailable(), available))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
