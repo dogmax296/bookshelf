019873accc197917193812c0ff38c5913eb95f53
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 14:00:19 +0300

    Add Genre Model

---
+++ src/main/java/com/dogmax/bookshelf/model/Genre.java
@@ -0,0 +1,14 @@
+package com.dogmax.bookshelf.model;
+
+public enum Genre {
+    MYSTERY,
+    SCIENCE_FICTION,
+    FANTASY,
+    ROMANCE,
+    THRILLER,
+    HISTORICAL_FICTION,
+    HORROR,
+    BIOGRAPHY,
+    SELF_HELP,
+    POETRY
+}
