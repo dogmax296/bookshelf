44ce3be3d9f65c07bb6966c73acb5523c17dd36c
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 15:55:47 +0300

    Add LanguageService

---
+++ src/main/java/com/dogmax/bookshelf/service/LanguageService.java
@@ -0,0 +1,18 @@
+package com.dogmax.bookshelf.service;
+
+import com.dogmax.bookshelf.model.Language;
+
+import java.util.List;
+import java.util.Map;
+
+public interface LanguageService {
+    Language create(Language language);
+
+    List<Language> findAll(Map<String, String> params);
+
+    Language getById(Long id);
+
+    void delete(Long id);
+
+    Language update(Language language);
+}
