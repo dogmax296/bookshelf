1c6338022e1474d277b3e8532f2b4dcb095a2c98
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 15:24:00 +0300

    Change Genre model

--- src/main/java/com/dogmax/bookshelf/model/Genre.java
+++ src/main/java/com/dogmax/bookshelf/model/Genre.java
@@ -1,14 +1,17 @@
 package com.dogmax.bookshelf.model;
 
-public enum Genre {
-    MYSTERY,
-    SCIENCE_FICTION,
-    FANTASY,
-    ROMANCE,
-    THRILLER,
-    HISTORICAL_FICTION,
-    HORROR,
-    BIOGRAPHY,
-    SELF_HELP,
-    POETRY
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String genre;
+
 }
