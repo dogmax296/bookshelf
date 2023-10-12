0a1b4f1cf23cf078778c2467e9059b3a9275c6fc
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-12 15:23:52 +0300

    Change Format model

--- src/main/java/com/dogmax/bookshelf/model/Format.java
+++ src/main/java/com/dogmax/bookshelf/model/Format.java
@@ -1,7 +1,16 @@
 package com.dogmax.bookshelf.model;
 
-public enum Format {
-    HARD,
-    SOFT,
-    EBOOK
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Format {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String format;
 }
