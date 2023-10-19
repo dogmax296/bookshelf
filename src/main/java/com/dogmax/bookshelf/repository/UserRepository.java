116f33d9498b6e3ae0773f9bbd9b5ce73315cd24
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-19 21:52:35 +0300

    Add Repositories

---
+++ src/main/java/com/dogmax/bookshelf/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.dogmax.bookshelf.repository;
+
+import com.dogmax.bookshelf.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
---
+++ src/main/java/com/dogmax/bookshelf/repository/WishlistRepository.java
@@ -0,0 +1,7 @@
+package com.dogmax.bookshelf.repository;
+
+import com.dogmax.bookshelf.model.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishlistRepository  extends JpaRepository<Wishlist,Long> {
+}
