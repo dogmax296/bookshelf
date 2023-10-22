a38c4ba6a981e9f78478cb565bd90382947ffa6a
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-22 17:35:17 +0300

    Add Interfaces for filtering

---
+++ src/main/java/com/dogmax/bookshelf/repository/specification/SpecificationProvider.java
@@ -0,0 +1,9 @@
+package com.dogmax.bookshelf.repository.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public interface SpecificationProvider<T> {
+    Specification<T> getSpecification(String[] params);
+
+    String getFilterKey();
+}
