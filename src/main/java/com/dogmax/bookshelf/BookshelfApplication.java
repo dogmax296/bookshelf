ea21158ea49811794599e9d8bc538e180f39c82b
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-10-28 19:01:18 +0300

    Remove CORS

--- src/main/java/com/dogmax/bookshelf/BookshelfApplication.java
+++ src/main/java/com/dogmax/bookshelf/BookshelfApplication.java
@@ -2,6 +2,9 @@ package com.dogmax.bookshelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BookshelfApplication {
@@ -9,14 +12,14 @@ public class BookshelfApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookshelfApplication.class, args);
     }
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*").allowedOrigins("*");
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*").allowedOrigins("*");
+            }
+        };
+    }
 
 }
